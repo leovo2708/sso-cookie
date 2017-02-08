15ea3fd2235c49dff9ed8b30179052fe690d9550
Leo Vo <leo.vo@nexcel.vn>
2017-02-08 16:41:50 +0700

    Init project.

---
+++ README.md
@@ -0,0 +1,3 @@
+SSO cookie solution:
+1. Login to SSO site & login with: demo / demo
+2. Go to WebApp1 site & it's ready loginned with demo account because they are using same cookie & machineID.
---
+++ SSO/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SSO.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+}
